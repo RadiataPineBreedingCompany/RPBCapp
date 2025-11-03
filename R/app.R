3c532725df1314358e40abd49842c2dffd52e646
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2025-11-03 11:36:44 -0500

    In browser

--- R/app.R
+++ R/app.R
@@ -1,9 +1,10 @@
 #' Run shiny application
 #' @import shiny
 #' @import bslib
+#' @param browser logical launch in browser
 #' @export
-run_app <- function()
+run_app <- function(browser = TRUE)
 {
   shiny::addResourcePath("www", system.file("app/www/", package = "RPBCapp"))
-  shiny::shinyApp(ui = ui, server = server)
+  shiny::shinyApp(ui = ui, server = server, options = list(launch.browser = browser))
 }
